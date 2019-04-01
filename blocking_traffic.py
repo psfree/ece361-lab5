6e8b0ebf679f299ef8e05409a9b67a07f4bf459b
Ubuntu <ubuntu@ece361.(none)>
2019-04-01 15:57:19 -0400

    ast changes

--- blocking_traffic.py
+++ blocking_traffic.py
@@ -1,6 +1,6 @@
 import ryu_ofctl
 
-dpid = 1
+dpid = '1'
 ryu_ofctl.deleteAllFlows(dpid)
 
 act2 = ryu_ofctl.OutputAction(2)
@@ -18,7 +18,7 @@ flow3 = ryu_ofctl.FlowEntry()
 flow3.dl_dst = "00:00:00:00:00:02"
 flow3.addAction(act2)
 
-dpid = 1
+
 
 ryu_ofctl.insertFlow(dpid, flow1)
 ryu_ofctl.insertFlow(dpid, flow2)
--- tapping_traffic.py
+++ tapping_traffic.py
@@ -6,18 +6,19 @@ act3 = ryu_ofctl.OutputAction(3)
 
 
 flow1 = ryu_ofctl.FlowEntry()
-flow1.dl_src = "00:00:00:00:00:01"
-flow1.dl_dst = "00:00:00:00:00:03"
+port1  = ryu_ofctl.getMacIngressPort("00:00:00:00:00:01")
+port2  = ryu_ofctl.getMacIngressPort("00:00:00:00:00:03")
+
+flow1.in_port = port1['port']
 flow1.addAction(act2)
 flow1.addAction(act3)
 
 flow2 = ryu_ofctl.FlowEntry()
-flow2.dl_src = "00:00:00:00:00:03"
-flow2.dl_dst = "00:00:00:00:00:01"
+flow2.in_port = port2['port']
 flow2.addAction(act1)
 flow2.addAction(act2)
 
-dpid = 1
+dpid = '1'
 
 ryu_ofctl.insertFlow(dpid, flow1)
 ryu_ofctl.insertFlow(dpid, flow2)
